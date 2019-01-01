e5ced0e9a501ffa03010733e1c4aa79dd0e11d78
sunyouzhi <sunyz@ieyecloud.com>
2019-01-01 16:30:28 +0800

    add

--- user-service-provider/src/main/java/com/lark/cloud/user/UserServiceProviderApplication.java
+++ user-service-provider/src/main/java/com/lark/cloud/user/UserServiceProviderApplication.java
@@ -2,9 +2,11 @@ package com.lark.cloud.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@EnableEurekaClient
 @MapperScan(basePackages = "com.lark.cloud.user.mapper") //mapper的包
 public class UserServiceProviderApplication {
     public static void main(String[] args) {
